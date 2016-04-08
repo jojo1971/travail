696e91fdfa94af511dd9ec700413a83a8bb80400
jojo1971 <shoun@free.fr>
2016-04-08 18:39:08 +0200

    popo

--- TP6.php
+++ TP6.php
@@ -1,15 +1,22 @@
-<?php header("content-type: text/javascript");
-$tab = file_get_contents('./towns.txt');
-$tab = unserialize($tab);
-for($i=0;$i<count($tab);$i++){
-    $string = $tab[i];
-    echo $string;
-    echo '<br />';
-}
-
-
-?>
-
-var lettre = '<?php echo $_GET['lettre']; ?>';
-
-receiveMessage(lettre);
+<?php
+    
+	$data = unserialize(file_get_contents('towns.txt')); // Récupération de la liste complète des villes
+	$dataLen = count($data);
+	
+	sort($data); // On trie les villes dans l'ordre alphabétique
+	
+	$results = array(); // Le tableau où seront stockés les résultats de la recherche
+	
+	// La boucle ci-dessous parcourt tout le tableau $data, jusqu'à un maximum de 10 résultats
+	
+	for ($i = 0 ; $i < $dataLen && count($results) < 10 ; $i++) {
+	    if (stripos($data[$i], $_GET['s']) === 0) { // Si la valeur commence par les mêmes caractères que la recherche
+	
+	        array_push($results, $data[$i]); // On ajoute alors le résultat à la liste à retourner
+	
+	    }
+	}
+	
+	echo implode('|', $results); // Et on affiche les résultats séparés par une barre verticale |
+	
+	?>
