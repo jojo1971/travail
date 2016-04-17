a37d8f2ce6627674864d6680eabf959dd28ae46a
jojo1971 <shoun@free.fr>
2016-04-17 17:04:36 +0200

    popo

--- test3.php
+++ test3.php
@@ -1,13 +1,14 @@
 <?php
 $towns = unserialize(file_get_contents('./towns.txt'));
-$townsLen = count($towns);
 sort($towns);
 
 $tab = array();
-for($i=0;$i<$townsLen && count($tab) < 10 ;$i++){
-    if(stripos($towns[$i],$_GET['s'] ===0)){
+for($i=0;$i<count($towns) && count($tab) < 10 ;$i++){
+    if(stripos($towns[$i], $_GET['s']) === 0){
         array_push($tab, $towns[$i]);
     }
 }
 echo implode('|', $tab);
+
+
 ?>
